56acacacf03c92bd98cd6ce5cce54692aebbefc9
Philip Rodrigues <philip@hypr.co.nz>
2022-08-16 11:30:28 +1200

    Updated script to grab the version.

--- update_build_extension.py
+++ update_build_extension.py
@@ -4,11 +4,15 @@ import yaml
 
 path = "stack/kustomize/base/01-deployment.yaml"
 name = "app-gateway"
-image = "phiroict/training_k8s_rust_gateway:20220813.11.0"
+image = "phiroict/training_k8s_rust_gateway:version"
 if len(sys.argv) > 3:
     path = sys.argv[1]
     name = sys.argv[2]
     image = sys.argv[3]
+version_path = image.split(":")[-1]
+image_version = open(version_path, 'r').readline().strip()
+image = image.split(":")[0] + ":" + image_version
+
 print("Start to inject the build number into the stack path: {}, name: {}, image: {}".format(path,name,image))
 configuration = None
 with open(path, 'r') as reader:
@@ -23,5 +27,4 @@ with open(path, 'r') as reader:
         out.append(doc)
     with open(path, "w") as writer:
         yaml.dump_all(out, writer, Dumper=yaml.Dumper)
-        writer.close()
 print("Image injection complete.")
